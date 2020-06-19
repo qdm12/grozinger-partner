bc8efd18abfad8e29ec6376951c5faf60c16b0a7
Quentin McGaw <quentin.mcgaw@gmail.com>
2020-06-19 18:39:51 -0400

    Update Google site verification code

--- src/components/MetaOwnership.tsx
+++ src/components/MetaOwnership.tsx
@@ -4,7 +4,7 @@ import { Helmet } from "react-helmet";
 function MetaOwnership(): JSX.Element {
   return (
     <Helmet>
-      <meta name="google-site-verification" content="w7XbTi6MOgw5-jljAKD87qxp7yWkwycE3PVP5NQ5F_8" />
+      <meta name="google-site-verification" content="ixyy3qwDLWDdBnHFEPEl834QhPw2ERmncoEj_utkF1E" />
       <meta name="author" content="GROZINGER & PARTNERS S.A." />
     </Helmet>
   );
