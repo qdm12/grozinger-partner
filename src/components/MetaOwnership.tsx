92b027deb3f97425184f5fec6eea7a3243380302
Quentin McGaw <quentin.mcgaw@gmail.com>
2020-07-03 10:53:48 -0400

    Change Google ownership

--- src/components/MetaOwnership.tsx
+++ src/components/MetaOwnership.tsx
@@ -4,7 +4,7 @@ import { Helmet } from "react-helmet";
 function MetaOwnership(): JSX.Element {
   return (
     <Helmet>
-      <meta name="google-site-verification" content="ixyy3qwDLWDdBnHFEPEl834QhPw2ERmncoEj_utkF1E" />
+      <meta name="google-site-verification" content="rkGV0R89-zTTiVDNjyd7G8RcUkiv5PfagCNhGVmuUZk" />
       <meta name="author" content="GROZINGER & PARTNERS S.A." />
     </Helmet>
   );
